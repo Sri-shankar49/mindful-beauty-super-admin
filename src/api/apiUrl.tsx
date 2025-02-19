5c1d6c8b8dd73b374765dda6ab709ba31340a188
Sri-shankar49 <srishankar4499@gmail.com>
2025-02-19 18:47:11 +0530

    commit feat : django URL changed

--- src/api/apiUrl.tsx
+++ src/api/apiUrl.tsx
@@ -4,8 +4,8 @@ import axios from 'axios';
 export const apiUrl = {
   // apiUrlConfig: "http://192.168.1.11:8000",
   // apiUrlConfig: "http://192.168.43.150:8000", Motorola One Power
-  apiUrlConfig: "https://mbrestapi-f8cphtgaf7fjdyb0.westcentralus-01.azurewebsites.net",    // Azure
-  // apiUrlConfig: "https://djangoapp-mindful-beautys-projects.vercel.app",
+  // apiUrlConfig: "https://mbrestapi-f8cphtgaf7fjdyb0.westcentralus-01.azurewebsites.net",    // Azure
+  apiUrlConfig: "https://djangoapp-mindful-beautys-projects.vercel.app",
 }
 
 // Create an Axios instance with the base URL
